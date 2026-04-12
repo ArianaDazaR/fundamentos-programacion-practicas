51df3252ba27cb932de4ed42d17a57d3b59532c6
ArianaDazaR <arianitadaza@gmail.com>
2026-04-12 19:30:52 -0400

    Add program to calculate sum of powers x^n

---
+++ Practica-3/P6-ExponenteNdeX.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main () {
+
+    int n, i;
+    float x;
+    int suma=0;
+
+    cout << "Ingrese un valor para x (x^n): ";
+    cin >> x;
+
+    cout << "Ingrese un valor para n (x^n): ";
+    cin >> n;
+
+    for (i=1; i<=n; i++){
+        suma = suma + pow(x, i);
+    }
+
+    cout << "\nLa suma de la serie, dada una base de " << x << ", elevada a los numeros enteros entre 1 y " << n << " es " << suma << endl;
+
+return 0;
+}
