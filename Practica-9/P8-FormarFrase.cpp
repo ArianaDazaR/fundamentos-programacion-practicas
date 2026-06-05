dfacdb2d75624965cc623bcf69995d75ed9f465b
ArianaDazaR <arianitadaza@gmail.com>
2026-06-05 00:23:15 -0400

    Create P8-FormarFrase.cpp

---
+++ Practica-9/P8-FormarFrase.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Cuantas palabras? ";
+    cin >> n;
+    cin.ignore();  // limpiar el buffer del enter
+
+    string frase = "";
+    for (int i = 0; i < n; i++) {
+        string palabra;
+        cout << "Palabra " << i+1 << ": ";
+        getline(cin, palabra);
+
+        if (i > 0) {
+            frase = frase + " ";
+        }
+        frase = frase + palabra;
+    }
+
+    cout << "Frase: " << frase << endl;
+    return 0;
+}
