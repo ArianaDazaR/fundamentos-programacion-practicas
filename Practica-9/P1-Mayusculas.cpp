0ee150783d67b63d13dbb52c34a2cdd6c9a5c2ea
ArianaDazaR <arianitadaza@gmail.com>
2026-06-04 22:35:49 -0400

    Create P1-Mayusculas.cpp

---
+++ Practica-9/P1-Mayusculas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string texto;
+    cout << "Ingrese un texto: ";
+    getline(cin, texto);
+
+    for (int i = 0; i < texto.length(); i++) {
+        texto[i] = toupper(texto[i]);
+    }
+
+    cout << "Texto en mayúsculas: " << texto << endl;
+    return 0;
+}
